0f31c497cd605fd1d9780201f6529cdfcb320bcb
hayek <hayek@HANNA-PC>
2018-12-01 19:02:06 +0800

    63. Creating Objects: Object.create

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,31 +1,53 @@
 //function constructor
 
-var Hanna={
-    name:'Hanna',
-    yearOfBirth:1984,
-    job:'Software'
+// var Hanna={
+//     name:'Hanna',
+//     yearOfBirth:1984,
+//     job:'Software'
+// };
+
+// var Person=function(name,yearOfBirth,job){
+//     this.name=name;
+//     this.yearOfBirth=yearOfBirth;
+//     this.job=job;
+   
+// }
+// //here we let the Person any person object inherit this method
+// Person.prototype.calculateAge=function(){
+//     console.log(2018-this.yearOfBirth);
+// }
+
+// Person.prototype.lastName='Smith';
+
+// var hanna=new Person('Hanna',1980,'software');
+// var jane=new Person('jane',1996,'designer');
+// var mark=new Person('mark',1948,'retired');
+// hanna.calculateAge();
+// jane.calculateAge();
+// mark.calculateAge();
+
+// console.log(jane.lastName);
+
+
+//Object.create
+
+var personProto={
+    calculateAge:function(){
+        console.log(2018-this.yearOfBirth);
+    }
 };
 
-var Person=function(name,yearOfBirth,job){
-    this.name=name;
-    this.yearOfBirth=yearOfBirth;
-    this.job=job;
-   
-}
-//here we let the Person any person object inherit this method
-Person.prototype.calculateAge=function(){
-    console.log(2018-this.yearOfBirth);
-}
+var hanna=Object.create(personProto);
 
-Person.prototype.lastName='Smith';
+hanna.name='hanna';
+hanna.yearOfBirth=1984;
+hanna.job='software';
 
-var hanna=new Person('Hanna',1980,'software');
-var jane=new Person('jane',1996,'designer');
-var mark=new Person('mark',1948,'retired');
-hanna.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
+var jane=Object.create(personProto,
+{
+    name:{value:'jane'},
+    yearOfBirth:{value:1996},
+    job:{value:'designer'}
 
-console.log(jane.lastName);
-
+});
 
