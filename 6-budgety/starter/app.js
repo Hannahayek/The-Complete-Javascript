b0ea2500ce5ae7f8ca414b057e535eb25354b9cc
hayek <hayek@HANNA-PC>
2018-12-22 22:14:25 +0800

    79. Reading Input Data

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -5,22 +5,43 @@ var budgetController=(function(){
 
 // UI Controller
 var UIController=(function(){
+var DOMstring={
+    inputType:'.add__type',
+    inputDescription:'.add__description',
+    inputValue:'.add__value',
+    inputBtn:'.add__btn'
+}
+  return {
+      getinput:function(){
+          return{
+            type:document.querySelector(DOMstring.inputType).value, //will be inc + or exp -
+            description:document.querySelector(DOMstring.inputDescription).value,
+            value:document.querySelector(DOMstring.inputValue).value
+          };
+      },
+      //here we make it public so can be accessed by other controllers
+      getDomStrings:function(){
+          return DOMstring;
+      }
+  }
 
 })();
 
 //GLOBAL APP Controller
 var controller=(function(budgetCtrl,UICtrl){
+var DOM=UIController.getDomStrings();
+
 var ctrlAddItem=function(){
 // 1. Get the filed input data
+var input=UICtrl.getinput();
+console.log(input);
 //2. add the item to the budget controller
 //3. add the item to the UI 
 //4. Calculate the budget
-//5. Display the budget in UI
-
-console.log("it works enter pressed");
+//5. Display the budget in U
 }
 
-document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
 
 
 
