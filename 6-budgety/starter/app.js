c28f13de1feeb9f7feaa55d1e7ad8fbda1aed2dd
hayek <hayek@HANNA-PC>
2018-12-22 21:38:01 +0800

    78. Setting up the First Event Listeners

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,26 +1,36 @@
+//BUDGET Controller
 var budgetController=(function(){
-var x=23;
-var add=function(a){
-    return x+a;
-}
-return {
-    publicTest:function(b){
-        console.log(add(b));
-    }
-}
+
 })();
 
+// UI Controller
 var UIController=(function(){
 
 })();
 
-
+//GLOBAL APP Controller
 var controller=(function(budgetCtrl,UICtrl){
-var z=budgetCtrl.publicTest(5);
+var ctrlAddItem=function(){
+// 1. Get the filed input data
+//2. add the item to the budget controller
+//3. add the item to the UI 
+//4. Calculate the budget
+//5. Display the budget in UI
 
-return{
-    anotherPublic:function(){
-        console.log(z);
-    }
+console.log("it works enter pressed");
 }
+
+document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+
+
+
+// the event will catch any key is pressed
+//key code we can get it from google keycode
+//keycode is enter event.which for older browsers
+document.addEventListener('keypress',function(event){
+    if(event.keyCode===13||event.which===13){
+        ctrlAddItem();
+    }
+});
+
 })(budgetController,UIController);
