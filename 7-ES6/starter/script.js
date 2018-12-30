1dc1a6ff0f43ebd993b55609f76cd711f441126b
Hanna Hayek (v-hahaye) <v-hahaye@ame.gbl>
2018-12-30 15:20:02 +0800

    104. Variable Declarations with let and const

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -1,4 +1,42 @@
 // Lecture: Let and const
 
 //ES5
-var name5='jane smith'
+/* var name5='jane smith';
+var age5=23;
+name5='jane miller';
+console.log(name5);
+
+//ES6
+const name6='jame smith';
+let age6=23;
+name6='jane miller';
+console.log(name6);
+ */
+//ES5
+
+function driverLicence5(passedTest){
+    if(passedTest){
+        var firstName='John';
+        var yearOfBirth=1990;
+        
+    }
+    console.log(firstName +','+'born in '+ yearOfBirth+',is now officially to drive a car');
+   
+}
+
+driverLicence5(true);
+
+//ES6
+
+function driverLicence6(passedTest){
+    let firstName;
+    const yearOfBirth =1990;
+    if(passedTest){
+         firstName='John';
+       
+        
+    }
+    console.log(firstName +','+'born in '+ yearOfBirth+',is now officially to drive a car');
+}
+
+driverLicence6(true);
