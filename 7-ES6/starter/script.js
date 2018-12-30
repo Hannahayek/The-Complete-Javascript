2664e836b23c83226734474f15362d4db84519e0
hayek <hayek@HANNA-PC>
2018-12-30 21:17:20 +0800

    108. Arrow Functions: Lexical 'this' Keyword

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -121,3 +121,76 @@ return `Age element ${index+1}: ${age}.`
 
 console.log(ages6);
 
+///////////////////////////
+//Lecture: Arrow Functions 2
+
+//ES5
+
+var box5={
+    color:'green',
+    position:1,
+    clickMe:function(){
+      var self=this; //if we use this instead of slef it will be undifined
+      document.querySelector('.green').addEventListener('click',function(){
+          var str='this is box number '+self.position+' and it is '+self.color; 
+          alert(str);
+    });
+    }
+}
+
+//box5.clickMe();
+
+//ES6
+
+const box6={
+    color:'green',
+    position:1,
+    clickMe:function(){
+      document.querySelector('.green').addEventListener('click',()=>{
+          var str='this is box number '+this.position+' and it is '+this.color; 
+          alert(str);
+    });
+    }
+}
+
+//box6.clickMe();
+
+/* const box66={ //this one will one work because of click me has arrow
+    color:'green',
+    position:1,
+    clickMe:()=>{
+      document.querySelector('.green').addEventListener('click',()=>{
+          var str='this is box number '+this.position+' and it is '+this.color; 
+          alert(str);
+    });
+    }
+}
+
+box66.clickMe(); */
+
+
+function Person(name){
+    this.name=name;
+}
+
+//ES5
+Person.prototype.myFriends5=function(fiends){
+    var arr=friends.map(function(el)
+    {
+    return this.name+' is friends with '+el;   
+    }.bind(this));  //we add bind(this) to point this to this function
+    console.log(arr);
+}
+var friends=['Bob','Jane','Mark'];
+new Person('John').myFriends5(friends);
+
+
+//ES6
+
+Person.prototype.myFriends6=function(fiends){
+    var arr=friends.map(el=>
+       `${this.name} is friends with ${el} `);
+ 
+    console.log(arr);
+}
+new Person('Mike').myFriends6(friends);
