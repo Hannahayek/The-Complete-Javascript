1c927da03c99a9fc44867c7f890ed92b1fe0c46d
hayek <hayek@HANNA-PC>
2019-01-02 21:36:16 +0800

    113. Default Parameters

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -341,3 +341,32 @@ function isFullAge6(limit,...years){
 years.forEach(cur => console.log((2018-cur)>=limit));
 }
 isFullAge6(21,1999,1965,2016,1987);
+
+
+///////////////////////////////
+//Lecture: Default Parameters
+/* //ES5
+function SmithPerson(firstName,yearOfBirth,lastName,nationality){
+    lastName===undefined ? lastName='Smith':lastName=lastName;
+    nationality===undefined ? nationality='american':nationality=nationality;
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.yearOfBirth=yearOfBirth;
+    this.nationality=nationality;
+}
+
+var john=new SmithPerson('john',1990);
+var emily=new SmithPerson('emily',1983,'Diaz','spanish') */
+
+
+//ES6 
+function SmithPerson(firstName,yearOfBirth,lastName='Smith',
+nationality='american'){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.yearOfBirth=yearOfBirth;
+    this.nationality=nationality;
+}
+
+var john=new SmithPerson('john',1990);
+var emily=new SmithPerson('emily',1983,'Diaz','spanish')
