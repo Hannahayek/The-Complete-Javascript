05104984f084b32b728951aa1e24d1cce528d6e6
Hanna Hayek (Pactera) <v-hahaye@microsoft.com>
2019-01-15 17:49:35 +0800

    143. Rendering an AJAX Loading Spinner

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import {elements,renderLoader,clearLoader} from './views/base';
 /**Global state of the app */
 /**-Search object */
 /**-Current recipe  object*/
@@ -19,9 +19,12 @@ const controlSearch= async ()=>{
        //3 Prepare UI for results
        searchView.clearInput();
        searchView.clearResults();
+     
+       renderLoader(elements.searchRes);
        //4) search for recipes 
        await state.search.getResults();
        //5) Render Results to UI
+       clearLoader();
       searchView.renderResults(state.search.result)
 
    }
