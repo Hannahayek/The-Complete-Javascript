b47b6b5a1bd519d1200059b375c2e001cb184796
hayek <hayek@HANNA-PC>
2019-01-20 22:16:48 +0800

    153. Building the Shopping List Controller

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -2,6 +2,7 @@ import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import {elements,renderLoader,clearLoader} from './views/base';
 import List from './models/List';
 /**Global state of the app */
@@ -10,6 +11,7 @@ import List from './models/List';
 /**-Shopping List  object*/
 /**- Liked recipes*/
 const state={};
+window.state=state;
 /**
  *SEARCH CONTROLLER
  */
@@ -108,6 +110,41 @@ window.addEventListener('load',controlRecipe); */
 
 ['hashchange','load'].forEach(event=>window.addEventListener(event,controlRecipe));
 
+
+/**
+ * LIST CONTROLLER
+ */
+  const controlList=()=>{
+     //create new list if there is none yet
+     if(!state.list) state.list=new List();
+     //add each ingredient to the list and UI
+     state.recipe.ingredients.forEach(el=>{
+        const item=state.list.addItem(el.count,el.unit,el.ingredient);
+        listView.renderItem(item);
+     });
+  }
+
+  //Handle delete and update list item events
+  elements.shopping.addEventListener('click',e=>{
+     const id=e.target.closest('.shopping__item').dataset.itemid;
+
+     //hndle the delete button
+
+     if(e.target.matches('.shopping__delete,.shopping__delete *')){
+        //delete from  state
+        state.list.deleteItem(id);
+
+        //delete from UI
+        listView.deleteItem(id);
+
+        //handle the count update
+     }else if(e.target.matches('.shopping__count-value')){
+         const val=parseFloat(e.target.value,10);
+         state.list.updateCount(id,val);
+     }
+
+  });
+
 //handling recipe button clicks
   elements.recipe.addEventListener('click',e=>{
       if(e.target.matches('.btn-decrease,.btn-decrease *')){
@@ -121,12 +158,14 @@ window.addEventListener('load',controlRecipe); */
          //icrease button is clicked
          state.recipe.updateServings('inc');
          recipeView.updateServingsIngredients(state.recipe);
+      }else if(e.target.matches('.recipe__btn--add,.recipe__btn--add *')){
+         controlList();
       }
-      console.log(state.recipe);
+      
   });
 
 
- window.l=new List();
+
 
 
 
--- 9-forkify/starter/src/js/models/List.js
+++ 9-forkify/starter/src/js/models/List.js
@@ -18,10 +18,10 @@ export default class List{
 
         //[2,4,8] splice(1,1) => return  4, original array is [2,8]
         //[2,4,8] slice(1,1) => return  4, original array is [2,4,8]
-        this.item.splice(index,1);
+        this.items.splice(index,1);
     }      
 
-    updateCount(id,newCount){
-         this.items.find(el=>el.id===id).count=newCount;
+    updateCount(id, newCount) {
+        this.items.find(el => el.id === id).count = newCount;
     }
 }
